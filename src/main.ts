2a950c83ef7c11044e5bf783ba1a2f4c65106ee3
Kowshik B <kavshikbabu7_bcs26@mepcoeng.ac.in>
2025-06-13 15:09:14 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
-import { provideRouter } from '@angular/router';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { appConfig } from './app/app.config';
+import { App } from './app/app';
 
-import { AppComponent } from './app/app.component';
-import { routes } from './app/app.routes';
-import { authInterceptor } from './app/core/interceptors/auth.interceptor';
-
-export default bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideAnimations(),
-    provideHttpClient(withInterceptors([authInterceptor])),
-  ],
-});
+bootstrapApplication(App, appConfig)
+  .catch((err) => console.error(err));
