6c66d042cc0f3de975f01b954fb954edc820b72c
Kowshik B <kavshikbabu7_bcs26@mepcoeng.ac.in>
2025-07-18 21:48:57 +0530

    Update booking.service.ts

--- src/app/core/services/booking.service.ts
+++ src/app/core/services/booking.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, tap } from 'rxjs';
 import { Booking } from '../models/booking.model';
-import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -12,40 +11,40 @@ export class BookingService {
   constructor(private http: HttpClient ) {}
   
   getAllBookings(): Observable<Booking[]> {
-    return this.http.get<Booking[]>(`${environment.apiUrl}/bookings`);
+    return this.http.get<Booking[]>(`${process.env.apiUrl}/bookings`);
   }
   
   getUserBookings(): Observable<Booking[]> {
-    return this.http.get<Booking[]>(`${environment.apiUrl}/bookings/user`);
+    return this.http.get<Booking[]>(`${process.env.apiUrl}/bookings/user`);
   }
   
   getBookingById(id: string): Observable<Booking> {
-    return this.http.get<Booking>(`${environment.apiUrl}/bookings/${id}`);
+    return this.http.get<Booking>(`${process.env.apiUrl}/bookings/${id}`);
   }
   
   createBooking(bookingData: FormData): Observable<Booking> {
-    return this.http.post<Booking>(`${environment.apiUrl}/bookings`, bookingData);
+    return this.http.post<Booking>(`${process.env.apiUrl}/bookings`, bookingData);
   }
   
   updateBooking(id: string, bookingData: any): Observable<Booking> {
-    return this.http.put<Booking>(`${environment.apiUrl}/bookings/${id}`, bookingData)
+    return this.http.put<Booking>(`${process.env.apiUrl}/bookings/${id}`, bookingData)
   }
 
   cancelBooking(id: string): Observable<any> {
-    return this.http.patch<any>(`${environment.apiUrl}/${id}/cancel`, {})
+    return this.http.patch<any>(`${process.env.apiUrl}/${id}/cancel`, {})
   }
   
   getActiveBookings(): Observable<Booking[]> {
-    return this.http.get<Booking[]>(`${environment.apiUrl}/bookings/active`);
+    return this.http.get<Booking[]>(`${process.env.apiUrl}/bookings/active`);
   }
   
   getBookingStatistics(): Observable<any> {
-    return this.http.get<any>(`${environment.apiUrl}/bookings/statistics`);
+    return this.http.get<any>(`${process.env.apiUrl}/bookings/statistics`);
   }
   processPayment(bookingId: string, paymentMethod: string): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/bookings/payments/process`, { bookingId })
+    return this.http.post(`${process.env.apiUrl}/bookings/payments/process`, { bookingId })
   }
   sendBookingEmail(email: string, pdfData: string): Observable<any> {
-    return this.http.post(`${environment.apiUrl}/bookings/send-email`, { email, pdfData });
+    return this.http.post(`${process.env.apiUrl}/bookings/send-email`, { email, pdfData });
   }
 }
