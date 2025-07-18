0c07dd8a48bc462b9f4829e0e87df193ced2e7a5
Kowshik B <kavshikbabu7_bcs26@mepcoeng.ac.in>
2025-07-18 22:07:38 +0530

    Update user.service.ts

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -10,33 +10,33 @@ export class UserService {
   constructor(private http: HttpClient) {}
   
   getAllUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${process.env.['['apiUrl']']}/users`);
+    return this.http.get<User[]>(`${process.env.['apiUrl']}/users`);
   }
   
   getUserById(id: string): Observable<User> {
-    return this.http.get<User>(`${process.env.['['apiUrl']']}/users/${id}`);
+    return this.http.get<User>(`${process.env.['apiUrl']}/users/${id}`);
   }
   
   createUser(userData: any): Observable<User> {
-    return this.http.post<User>(`${process.env.['['apiUrl']']}/users`, userData);
+    return this.http.post<User>(`${process.env.['apiUrl']}/users`, userData);
   }
   
   updateUser(id: string, userData: any): Observable<User> {
-    return this.http.put<User>(`${process.env.['['apiUrl']']}/users/${id}`, userData);
+    return this.http.put<User>(`${process.env.['apiUrl']}/users/${id}`, userData);
   }
   
   deleteUser(id: string): Observable<any> {
-    return this.http.delete(`${process.env.['['apiUrl']']}/users/${id}`);
+    return this.http.delete(`${process.env.['apiUrl']}/users/${id}`);
   }
   
   promoteToAdmin(id: string): Observable<User> {
-    return this.http.patch<User>(`${process.env.['['apiUrl']']}/users/${id}/promote`, {});
+    return this.http.patch<User>(`${process.env.['apiUrl']}/users/${id}/promote`, {});
   }
   
   demoteFromAdmin(id: string): Observable<User> {
-    return this.http.patch<User>(`${process.env.['['apiUrl']']}/users/${id}/demote`, {});
+    return this.http.patch<User>(`${process.env.['apiUrl']}/users/${id}/demote`, {});
   }
   createAdmin(admin: User): Observable<User> {
-    return this.http.post<User>(`${process.env.['['apiUrl']']}/admin`, admin);
+    return this.http.post<User>(`${process.env.['apiUrl']}/admin`, admin);
   }
 }
