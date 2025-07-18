744d9e68d6ea2fa004864e7ed53fce7a07f21f3f
Kowshik B <kavshikbabu7_bcs26@mepcoeng.ac.in>
2025-07-18 21:56:05 +0530

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  baseUrl: 'https://agroequip-express-backend.onrender.com',
-  apiUrl: 'https://agroequip-express-backend.onrender.com/api'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  baseUrl: 'https://agroequip-express-backend.onrender.com',
-  apiUrl: 'https://agroequip-express-backend.onrender.com/api'
-};
